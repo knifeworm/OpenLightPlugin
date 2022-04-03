355f729e1938c31525633d93aee5c0d695126f6b
knifeworm <77696553+knifeworm@users.noreply.github.com>
2022-04-03 10:08:31 +0100

    Update OpenLight.java
    Just adding the basic structure to the main file.

--- src/main/java/me/knifeworm/openlight/OpenLight.java
+++ src/main/java/me/knifeworm/openlight/OpenLight.java
@@ -1,17 +1,32 @@
 package me.knifeworm.openlight;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OpenLight extends JavaPlugin {
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
-
+    public void onEnable()
+    {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "OpenLight has loaded!");
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onDisable()
+    {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "OpenLight has been disabled!");
+    }
+
+    // Loads all the commands.
+    public void loadCommands()
+    {
+
+    }
+
+    // Loads all the events.
+    public void loadEvents()
+    {
+
     }
 }
