c94117a4e6ec027b2171abd188ed550e832f2972
knifeworm <77696553+knifeworm@users.noreply.github.com>
2022-04-04 19:21:31 +0100

    Starting to add permissions.

---
+++ src/main/java/me/knifeworm/openlight/group/GroupConfig.java
@@ -0,0 +1,11 @@
+package me.knifeworm.openlight.group;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GroupConfig {
+
+}
---
+++ src/main/java/me/knifeworm/openlight/permission/PermissionStatus.java
@@ -0,0 +1,24 @@
+package me.knifeworm.openlight.permission;
+
+public enum PermissionStatus
+{
+    ALLOWED(true),
+    DECLINED(false),
+    NOT_SET(false);
+
+    private final boolean booleanValue;
+
+    PermissionStatus(boolean booleanValue){
+        this.booleanValue = booleanValue;
+    }
+
+    public static PermissionStatus of(boolean booleanValue){
+        return booleanValue ? ALLOWED : DECLINED;
+    }
+
+    public boolean booleanValue(){
+        return booleanValue;
+    }
+
+
+}
