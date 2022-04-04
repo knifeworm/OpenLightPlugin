553e9e927ae494a1950520a42dd2e59e70d96895
knifeworm <77696553+knifeworm@users.noreply.github.com>
2022-04-04 05:21:20 +0100

    Starting to add support for an anticheat

---
+++ src/main/java/me/knifeworm/openlight/util/MaterialCheck.java
@@ -0,0 +1,13 @@
+package me.knifeworm.openlight.util;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+public abstract class MaterialCheck
+{
+    public final boolean checkMaterial(Location location){
+        return checkMaterial(location.getBlock().getType());
+    }
+
+    public abstract boolean checkMaterial(Material material);
+}
