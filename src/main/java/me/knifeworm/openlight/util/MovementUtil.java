3683939c7a441e7bee23cb22e543042d9f5a1ada
knifeworm <77696553+knifeworm@users.noreply.github.com>
2022-04-04 06:00:38 +0100

    Finished off the util package

---
+++ src/main/java/me/knifeworm/openlight/util/MovementUtil.java
@@ -0,0 +1,106 @@
+package me.knifeworm.openlight.util;
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+public class MovementUtil {
+
+    public static final double PLAYER_WIDTH = .6;
+    public static final double PLAYER_HEIGTH = 1.8;
+    public static final double PLAYER_HEIGTH_SNEAKING = 1.6;
+    private static final MaterialCheck CHECK_STAIR, CHECK_STEP;
+
+    public static boolean isStepping(Location location) {
+        return isColliding(location, CHECK_STAIR) || isColliding(location, CHECK_STEP);
+    }
+
+    public static boolean isColliding(Location location, MaterialCheck material) {
+        double d = PLAYER_WIDTH/2;
+        return material.checkMaterial(location)
+                || material.checkMaterial(location.clone().add( d, 0, 0))
+                || material.checkMaterial(location.clone().add(-d, 0, 0))
+                || material.checkMaterial(location.clone().add( d, 0,  d))
+                || material.checkMaterial(location.clone().add(-d, 0,  d))
+                || material.checkMaterial(location.clone().add( d, 0, -d))
+                || material.checkMaterial(location.clone().add(-d, 0, -d))
+                || material.checkMaterial(location.clone().add(0,  0, -d))
+                || material.checkMaterial(location.clone().add(0,  0,  d));
+    }
+
+    /**
+     * This returns if the player should be flagged
+     * @return true if the player is now on ground or if the player is on a vine or on a ladder (Liquids?)
+     * */
+    public static boolean shouldNotFlag(Location loc) {
+        return isMaterialGlideable(loc.getBlock().getType())
+                || isMaterialGlideable(loc.clone().add(PLAYER_WIDTH/2, 0, 0).getBlock().getType())
+                || isMaterialGlideable(loc.clone().add(PLAYER_WIDTH/2, 0, PLAYER_WIDTH/2).getBlock().getType())
+                || isMaterialGlideable(loc.clone().add(PLAYER_WIDTH/2, 0, -PLAYER_WIDTH/2).getBlock().getType())
+                || isMaterialGlideable(loc.clone().add(-PLAYER_WIDTH/2, 0, 0).getBlock().getType())
+                || isMaterialGlideable(loc.clone().add(-PLAYER_WIDTH/2, 0, -PLAYER_WIDTH/2).getBlock().getType())
+                || isMaterialGlideable(loc.clone().add(0, 0, -PLAYER_WIDTH/2).getBlock().getType())
+                || isMaterialGlideable(loc.clone().add(0, 0, PLAYER_WIDTH/2).getBlock().getType())
+                || isOnGround(loc.clone().add(0, 0, 0));
+
+    }
+
+    public static boolean isOnGround(Location loc) {
+        return loc.getBlock().getLocation().clone().add(0, -1, 0).getBlock().getType().isSolid() || loc.clone().add(PLAYER_WIDTH/2, -1,0).getBlock().getType().isSolid()
+                || loc.clone().add(PLAYER_WIDTH/2, -1, PLAYER_WIDTH/2).getBlock().getType().isSolid()
+                || loc.clone().add(PLAYER_WIDTH/2, -1, -PLAYER_WIDTH/2).getBlock().getType().isSolid()
+                || loc.clone().add(-PLAYER_WIDTH/2, -1, 0).getBlock().getType().isSolid()
+                || loc.clone().add(-PLAYER_WIDTH/2, -1,-PLAYER_WIDTH/2).getBlock().getType().isSolid()
+                || loc.clone().add(0, -1,-PLAYER_WIDTH/2).getBlock().getType().isSolid()
+                || loc.clone().add(0, -1, PLAYER_WIDTH/2).getBlock().getType().isSolid();
+    }
+
+    public static boolean isMaterialGlideable(Material mat) {
+        // TODO Add liquids
+        switch (mat) {
+            case LADDER:
+            case VINE:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    static {
+        CHECK_STAIR = new MaterialCheck() {
+
+            @Override
+            public boolean checkMaterial(Material material) {
+                switch(material){
+                    case ACACIA_STAIRS:
+                    case BRICK_STAIRS:
+                    case BIRCH_STAIRS:
+                    case COBBLESTONE_STAIRS:
+                    case DARK_OAK_STAIRS:
+                    case JUNGLE_STAIRS:
+                    case NETHER_BRICK_STAIRS:
+                    case QUARTZ_STAIRS:
+                    case RED_SANDSTONE_STAIRS:
+                    case SANDSTONE_STAIRS:
+                    case SMOOTH_QUARTZ_STAIRS:
+                    case SPRUCE_STAIRS:
+                    case OAK_STAIRS:
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        };
+        CHECK_STEP = new MaterialCheck() {
+
+            @Override
+            public boolean checkMaterial(Material material) {
+                switch(material){
+                    case OAK_STAIRS:
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        };
+    }
+
+}
---
+++ src/main/java/me/knifeworm/openlight/util/User.java
@@ -0,0 +1,80 @@
+package me.knifeworm.openlight.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
+
+
+public class User
+{
+    private final Player player;
+    public double oldY = 0;
+    private ArrayList<Long> hits = new ArrayList<>();
+    private HashMap<Long, Integer> entities = new HashMap<>();
+    private long lastTimeHitsCleaned = 0, lastTimeEntitiesCleaned = 0;
+
+    public boolean wasGoingUp = false;
+    public int oldYModifier = 0, ticksUp = 0, oldTicksUp = 0;
+
+    public User(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer(){
+        return player;
+    }
+
+    public void addHit(){
+        hits.add(System.currentTimeMillis());
+    }
+
+    public int getHits(){
+        long start = System.currentTimeMillis();
+        ArrayList<Long> toRemove = new ArrayList<>();
+        int result = 0;
+        for(long l : hits)
+            if(start - l > 1000L)
+                toRemove.add(l);
+            else
+                result++;
+        hits.removeAll(toRemove);
+        toRemove.clear();
+        lastTimeHitsCleaned = start;
+        return result;
+    }
+
+    public void addEntity(int i){
+        entities.put(System.currentTimeMillis(), i);
+    }
+
+    public int getEntities() {
+        long start = System.currentTimeMillis();
+        ArrayList<Long> toRemove = new ArrayList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        int result = 0;
+        for(long l : entities.keySet()){
+            int entityId = entities.get(1);
+            if(start - l > 1000L)
+                toRemove.add(l);
+            else if(!res.contains(entityId)){
+                result++;
+                res.add(entityId);
+            }
+        }
+        hits.removeAll(toRemove);
+        toRemove.clear();
+        res.clear();
+        lastTimeEntitiesCleaned = start;
+        return result;
+    }
+
+    public long getLastTimeHitsCleaned() {
+        return lastTimeHitsCleaned;
+    }
+
+    public long getLastTimeEntitiesCleaned()
+    {
+        return lastTimeEntitiesCleaned;
+    }
+}
